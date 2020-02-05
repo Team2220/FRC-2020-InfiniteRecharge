b4eacbf51e173387d1ab1f753f55889b11fa87bf
Reece Holmdahl <rholmdahl@gmail.com>
2020-02-04 19:21:12 -0600

    Implemented basic shooter functionality

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,27 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static final class ShooterConstants {
+
+        public static final int LEFT_FALCON = 1;
+        public static final int RIGHT_FALCON = 2;
+
+        public static final double RAMP_RATE = 1.5;
+
+        public static final NeutralMode IDLE_BEHAVIOR = NeutralMode.Coast;
+    }
 }
