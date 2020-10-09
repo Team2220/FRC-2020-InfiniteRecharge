f1d18b4845298552b7c6edb3be1b349434bd04d2
Thanuboyapati <46584322+Thanuboyapati@users.noreply.github.com>
2020-10-08 20:03:33 -0500

    Print date and time
    Also miliseconds. By Olma Wakjira, Gabe Trivino.

--- src/main/java/frc/robot/util/Logger.java
+++ src/main/java/frc/robot/util/Logger.java
@@ -1,7 +1,14 @@
 package frc.robot.util;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Logger {
     public static void log(String message){
-        System.out.println(message);
+        // using Date class
+        Date date = new Date();
+        System.out.println(sdf.format(date)+" " + message  );
     }
+    private static final DateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss:SSSS");   
 }
