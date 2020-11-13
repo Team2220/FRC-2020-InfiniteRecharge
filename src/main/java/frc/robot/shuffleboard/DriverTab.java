a23a21f8aa54f0299c8b93820e70178f3c809f8d
Thanuboyapati <46584322+Thanuboyapati@users.noreply.github.com>
2020-11-12 19:44:00 -0600

    made room for 2 USB cameras

--- src/main/java/frc/robot/shuffleboard/DriverTab.java
+++ src/main/java/frc/robot/shuffleboard/DriverTab.java
@@ -3,16 +3,25 @@ package frc.robot.shuffleboard;
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class DriverTab {
     private ShuffleboardTab tab = Shuffleboard.getTab("Driver");
 
-    public DriverTab(){
-           
-        MjpegServer server = CameraServer.getInstance().addServer("USB server 0");
-        UsbCamera camera = new UsbCamera("USB Camera 0", 0);
+    public DriverTab() {
+
+        UsbCamera camera = makeUSBCamera(0);
+        tab.add(camera).withSize(7, 7).withPosition(0, 0);
+
+        UsbCamera camera1 = makeUSBCamera(1);
+        tab.add(camera1).withSize(7, 7).withPosition(7, 0);
+    
+    }
+    private UsbCamera makeUSBCamera(int device){
+        MjpegServer server = CameraServer.getInstance().addServer("USB server "+ device);
+        UsbCamera camera = new UsbCamera("USB Camera " + device, device);
         CameraServer.getInstance().addCamera(camera);
         server.setSource(camera);
         server.getProperty("compression").set(10);
@@ -22,7 +31,6 @@ public class DriverTab {
         camera.setResolution(320, 240);
         server.getProperty("fps").set(22);
         camera.setFPS(22);
-        tab.add(camera).withSize(7, 7).withPosition(0, 0);
-    
+        return camera;
     }
 }
